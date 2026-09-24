69400fc12902a9d458cce321df09c750914b982a
BhairavK <bhairavk.gitacc@gmail.com>
2026-09-24 16:48:11 +0530

    Configure Prisma PostgreSQL SSL

--- server/src/config/prisma.js
+++ server/src/config/prisma.js
@@ -3,8 +3,10 @@ import { PrismaClient } from "../../generated/prisma/client.ts";
 
 const adapter = new PrismaPg({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
-
 const prisma = new PrismaClient({
     adapter,
 });
